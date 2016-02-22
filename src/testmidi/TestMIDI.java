f98a793fc257f8048644fa8c22539530f684dcbd
juhnowski <juhnowski@gmail.com>
2016-02-22 03:43:52 +0300

    Add dump receiver

--- src/testmidi/TestMIDI.java
+++ src/testmidi/TestMIDI.java
@@ -1,17 +1,35 @@
 package testmidi;
 
 import java.util.Vector;
+import javax.sound.midi.ControllerEventListener;
 import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MetaEventListener;
+import javax.sound.midi.MetaMessage;
 import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Receiver;
+import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Synthesizer;
+import javax.sound.midi.Track;
 import javax.sound.midi.Transmitter;
 
-public class TestMIDI {
+public class TestMIDI implements ControllerEventListener {
+
+    private static volatile TestMIDI instance;
+
+    public static TestMIDI getInstance() {
+        if (instance == null) {
+            synchronized (TestMIDI.class) {
+                if (instance == null) {
+                    instance = new TestMIDI();
+                }
+            }
+        }
+        return instance;
+    }
 
     public static void main(String[] args) throws InvalidMidiDataException {
         Vector synthInfos = new Vector();
@@ -20,12 +38,15 @@ public class TestMIDI {
         MidiDevice casioExt = null;
         MidiDevice oracle = null;
 
-        Sequencer seq;
+        Sequencer sequencer;
         Transmitter trans = null;
+        Transmitter trans1 = null;
 
         String casioName = "CASIO USB-MIDI";
         String casioDescription = "External MIDI Port";
         String oracleRTS = "Real Time Sequencer";
+
+        TestMIDI tm = getInstance();
 
         MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
         for (int i = 0; i < infos.length; i++) {
@@ -72,7 +93,7 @@ public class TestMIDI {
         }
 
         System.out.println("Send message");
-        
+
         ShortMessage myMsg = new ShortMessage();
         myMsg.setMessage(ShortMessage.NOTE_ON, 0, 60, 93);
         long timeStamp = -1;
@@ -80,22 +101,27 @@ public class TestMIDI {
             Receiver rcvr = casioExt.getReceiver();
             rcvr.send(myMsg, timeStamp);
             trans = casio.getTransmitter();
-            
-            
+            trans1 = casio.getTransmitter();
+
         } catch (MidiUnavailableException e) {
             e.printStackTrace();
         }
-         
 
-        ShortMessage myMsg1 = new ShortMessage();
-        myMsg.setMessage(ShortMessage.NOTE_ON, 0, 60, 93);
-        long timeStamp1 = -1;
         try {
             Receiver rcvr = MidiSystem.getReceiver();
-            trans.setReceiver(rcvr);
+            trans1.setReceiver(rcvr);
+            
+            DumpReceiver dr = new DumpReceiver(System.out);
+            trans.setReceiver(dr);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void controlChange(ShortMessage sm) {
+        System.out.println(sm.toString());
     }
 
 }
